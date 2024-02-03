64f2639ac2e1dea3610ff113c0229b4e11ff4333
densam4754 <rudonyasamson@gmail.com>
2024-02-03 01:35:31 -0800

    commit

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
-    database_name:"chais_db",
+    database_name:"chaisdb",
     password:"",
     port:"3306",
 
